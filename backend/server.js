ac175f7daeae247aceda8f73ae495de1526f4002
KAMLESH KUMAR SHARMA <kamleshsamudih2003@gmail.com>
2025-09-25 18:16:34 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,9 @@ app.use(cors({
     'http://localhost:5173', 
     'http://localhost:5174', 
     'https://achhadam-frontend.onrender.com',
-    'https://achhadamf.onrender.com'
+    'https://achhadamf.onrender.com',
+    'https://acchadam1.onrender.com',
+    'https://acchadam1-frontend.onrender.com'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
